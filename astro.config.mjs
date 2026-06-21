3fbb99e2052988cb62dc9ca0e93b2c28f86419ee
Tomohiro Ishii <tommy.ish@icloud.com>
2026-06-21 12:41:57 +0900

    新しいコンポーネント（Header、Footer、Menu、Navigation、BlogPost、Social）を追加し、ページレイアウトを改善しました。また、RSSフィードの生成とタグインデックスページを実装しました。依存関係に@astrojs/rssを追加し、各ページのスタイルを整えました。

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://iridescent-kulfi-98edd6.netlify.app'
+});
